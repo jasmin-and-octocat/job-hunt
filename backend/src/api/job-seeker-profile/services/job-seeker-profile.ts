56e6e68a66d4eb44b908cc698e6d0846af879647
jasmin-and-octocat <206257439+jasmin-and-octocat@users.noreply.github.com>
2025-04-05 02:45:21 +0530

    Update job-seeker-profile.ts

--- backend/src/api/job-seeker-profile/services/job-seeker-profile.ts
+++ backend/src/api/job-seeker-profile/services/job-seeker-profile.ts
@@ -4,4 +4,6 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::job-seeker-profile.job-seeker-profile');
+export default factories.createCoreService(
+  'api::job-seeker-profile.job-seeker-profile'
+);
